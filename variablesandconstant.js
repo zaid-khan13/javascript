b6fe17c1928ff6acf61170d7fa777a3a61174318
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-03-03 15:18:11 -0500

    created constants and variables

---
+++ variablesandconstant.js
@@ -0,0 +1,18 @@
+/*constants*/
+const brand = "toyota"
+const model = "revo"
+const fueltype = "diesel"
+const enginecapacity= 2.8
+const topspeed = 180
+const drivetrain= "4X4"
+const bodytype = "pickup"
+
+/*variables*/
+let color = "black"
+let rimscolor = "silver"
+let seats = "thin and uncomfortable"
+let interior = "black"
+let horsepower = 201
+let suspension = "hard"
+let headlights = "h4 led headlights"
+
