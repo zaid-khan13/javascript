9aafaf21c50a3475946204c6502c1d9c8e5418fb
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-03-04 15:22:47 -0500

    added more information

--- variablesandconstant.js
+++ variablesandconstant.js
@@ -6,6 +6,11 @@ const enginecapacity= 2.8
 const topspeed = 180
 const drivetrain= "4X4"
 const bodytype = "pickup"
+const vehicleweight = "2910kg"
+const fueltankcapacity = "80l"
+const groundclearence = 310
+const seatingcapacity = 5
+const sidemirrors = "retracable"
 
 /*variables*/
 let color = "black"
@@ -15,4 +20,13 @@ let interior = "black"
 let horsepower = 201
 let suspension = "hard"
 let headlights = "h4 led headlights"
+let airbags = 2
+let climatecontrol = "manual"
+let rearglass = "sliding"
+let frontgrill = "silver"
+let transmission = "auto"
 
+
+
+
+// this is a black toyota revo 
