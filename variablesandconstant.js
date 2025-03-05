cdc2134e3ee4f503482dd49b37d2eeea746f9b94
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-03-05 15:24:39 -0500

    added description

--- variablesandconstant.js
+++ variablesandconstant.js
@@ -1,32 +1,37 @@
 /*constants*/
-const brand = "toyota"
-const model = "revo"
-const fueltype = "diesel"
-const enginecapacity= 2.8
-const topspeed = 180
-const drivetrain= "4X4"
-const bodytype = "pickup"
-const vehicleweight = "2910kg"
-const fueltankcapacity = "80l"
-const groundclearence = 310
-const seatingcapacity = 5
-const sidemirrors = "retracable"
+const brand = "toyota";
+const model = "revo";
+const fueltype = "diesel";
+const enginecapacity = 2.8;
+const topspeed = 180;
+const drivetrain = "4X4";
+const bodytype = "pickup";
+const vehicleweight = "2910kg";
+const fueltankcapacity = "80l";
+const groundclearence = 310;
+const seatingcapacity = 5;
+const sidemirrors = "retracable";
 
 /*variables*/
-let color = "black"
-let rimscolor = "silver"
-let seats = "thin and uncomfortable"
-let interior = "black"
-let horsepower = 201
-let suspension = "hard"
-let headlights = "h4 led headlights"
-let airbags = 2
-let climatecontrol = "manual"
-let rearglass = "sliding"
-let frontgrill = "silver"
-let transmission = "auto"
+let color = "black";
+let rimscolor = "silver";
+let seats = "thin and uncomfortable";
+let interior = "black";
+let horsepower = 201;
+let suspension = "hard";
+let headlights = "h4 led headlights";
+let airbags = 2;
+let climatecontrol = "manual";
+let rearglass = "sliding";
+let frontgrill = "silver";
+let transmission = "auto";
 
-
-
-
-// this is a black toyota revo 
+// this is a black toyota revo with diesel enigine 2.8 the top speed of
+// this car is 180 the drivetrain is 4x4 and bodytype is pickup and the
+// weight of this vehicle is 2910, fuel tank capacity is 80l, ground clearence
+// is 310, seating capacity is for 5 persons, side mirrors are retractable,
+// rims are in silver color, seats are uncomfortable and thin, interior is in
+// black color, horsepower of this car is 201, suspension is hard, headlights
+// are h4led, there are 2 airbags in this car, if we talk about cimate control,
+// climate control is manual, rearglass is provided in sliding, frontgrill is
+// silver and transmission is Auto.
