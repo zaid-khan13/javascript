f91532758d62ca22a00069864b65b270724e9c4e
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-03-07 15:07:41 -0500

    changes in print text

--- variablesandconstant.js
+++ variablesandconstant.js
@@ -26,12 +26,31 @@ let rearglass = "sliding";
 let frontgrill = "silver";
 let transmission = "auto";
 
-// this is a black toyota revo with diesel enigine 2.8 the top speed of
-// this car is 180 the drivetrain is 4x4 and bodytype is pickup and the
-// weight of this vehicle is 2910, fuel tank capacity is 80l, ground clearence
-// is 310, seating capacity is for 5 persons, side mirrors are retractable,
-// rims are in silver color, seats are uncomfortable and thin, interior is in
-// black color, horsepower of this car is 201, suspension is hard, headlights
-// are h4led, there are 2 airbags in this car, if we talk about cimate control,
-// climate control is manual, rearglass is provided in sliding, frontgrill is
-// silver and transmission is Auto.
+console.log(`this is a black toyota revo with diesel enigine 2.8 the top speed of
+ this car is 180 the drivetrain is 4x4 and bodytype is pickup and the
+ weight of this vehicle is 2910, fuel tank capacity is 80l, ground clearence
+ is 310, seating capacity is for 5 persons, side mirrors are retractable,
+ rims are in silver color, seats are uncomfortable and thin, interior is in
+ black color, horsepower of this car is 201, suspension is hard, headlights
+ are h4led, there are 2 airbags in this car, if we talk about cimate control,
+ climate control is manual, rearglass is provided in sliding, frontgrill is
+ silver and transmission is Auto.`);
+
+
+
+console.log(`
+    this is a ${brand} ${model} with ${fueltype} engine ${enginecapacity} the top speed of this car is ${topspeed} and drivetrain is ${drivetrain} the bodytype is ${bodytype} the weight of this vehicle is ${vehicleweight} fuel tank capacity is ${fueltankcapacity} 
+    the groundclearance is ${groundclearence} seating capacity is for ${seatingcapacity} sidemirrors are ${sidemirrors} the color of this vehicle is ${color} and rims are in ${rimscolor} seats are ${seats}
+    interior color is ${interior} the horse power is ${horsepower} suspension is ${suspension} headlights are ${headlights} airbags are ${airbags} climate control is ${climatecontrol}
+    rearglass is provided in ${rearglass} frontgrill is ${frontgrill} and the transmission is ${transmission}`);
+
+
+//  this is a black toyota revo with diesel enigine 2.8 the top speed of
+//  this car is 180 the drivetrain is 4x4 and bodytype is pickup and the
+//  weight of this vehicle is 2910, fuel tank capacity is 80l, ground clearence
+//  is 310, seating capacity is for 5 persons, side mirrors are retractable,
+//  rims are in silver color, seats are uncomfortable and thin, interior is in
+//  black color, horsepower of this car is 201, suspension is hard, headlights
+//  are h4led, there are 2 airbags in this car, if we talk about cimate control,
+//  climate control is manual, rearglass is provided in sliding, frontgrill is
+//  silver and transmission is Auto.
