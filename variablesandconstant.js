d548479c77a316e472f08c6f77dcfdd86b16d56f
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-03-10 15:29:33 -0400

    changes in print

--- variablesandconstant.js
+++ variablesandconstant.js
@@ -30,15 +30,15 @@ console.log(`this is a black toyota revo with diesel enigine 2.8 the top speed o
  this car is 180 the drivetrain is 4x4 and bodytype is pickup and the
  weight of this vehicle is 2910, fuel tank capacity is 80l, ground clearence
  is 310, seating capacity is for 5 persons, side mirrors are retractable,
- rims are in silver color, seats are uncomfortable and thin, interior is in
+ rims are in silver color, seats are thin and uncomfortable, interior is in
  black color, horsepower of this car is 201, suspension is hard, headlights
  are h4led, there are 2 airbags in this car, if we talk about cimate control,
  climate control is manual, rearglass is provided in sliding, frontgrill is
- silver and transmission is Auto.`);
+ silver and the transmission is Auto.`);
 
 
 
-console.log(`
+console.log(` 
     this is a black ${brand} ${model} with ${fueltype} engine ${enginecapacity} the top speed of this car is ${topspeed} the drivetrain is ${drivetrain} and bodytype is ${bodytype} and the weight of this vehicle is ${vehicleweight} fuel tank capacity is ${fueltankcapacity} 
     groundclearance is ${groundclearence} seating capacity is for ${seatingcapacity} persons, sidemirrors are ${sidemirrors} and rims are in ${rimscolor} color, seats are ${seats}
     interior is in ${interior} color, horse power of this car is ${horsepower} suspension is ${suspension} headlights are ${headlights} there are ${airbags} airbags in this car, if we talk about cimate control, climate control is ${climatecontrol}
