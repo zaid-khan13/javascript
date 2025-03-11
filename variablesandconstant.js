0bcb58aac8b112dfaaafc5506f558ca15cdd62c2
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-03-11 15:33:34 -0400

    changes in print

--- variablesandconstant.js
+++ variablesandconstant.js
@@ -25,27 +25,27 @@ let rearglass = "sliding";
 let frontgrill = "silver";
 let transmission = "Auto";
 
-console.log(`this is a black toyota revo with diesel enigine 2.8 the top speed of
+console.log(`this is a black toyota revo with diesel engine 2.8 the top speed of
  this car is 180 the drivetrain is 4x4 and bodytype is pickup and the
  weight of this vehicle is 2910, fuel tank capacity is 80l, ground clearence
  is 310, seating capacity is for 5 persons, side mirrors are retractable,
  rims are in silver color, seats are thin and uncomfortable, interior is in
  black color, horsepower of this car is 201, suspension is hard, headlights
- are h4led, there are 2 airbags in this car, if we talk about cimate control,
+ are h4led, there are 2 airbags in this car, if we talk about climate control,
  climate control is manual, rearglass is provided in sliding, frontgrill is
  silver and the transmission is Auto.`);
 
 
 
 console.log(`this is a black ${brand} ${model} with ${fueltype} engine ${enginecapacity} the top speed of
-this car is ${topspeed} the drivetrain is ${drivetrain} and bodytype is ${bodytype} and the
-weight of this vehicle is ${vehicleweight}, fuel tank capacity is ${fueltankcapacity}, ground clearance 
-is ${groundclearence}, seating capacity is for ${seatingcapacity} persons, sidemirrors are ${sidemirrors},
-rims are in ${rimscolor} color, seats are ${seats}, interior is in
-${interior} color, horse power of this car is ${horsepower}, suspension is ${suspension}, headlights 
-are ${headlights}, there are ${airbags} airbags in this car, if we talk about climate control,
-climate control is ${climatecontrol}, rearglass is provided in ${rearglass}, frontgrill is
-${frontgrill} and the transmission is ${transmission}`);
+ this car is ${topspeed} the drivetrain is ${drivetrain} and bodytype is ${bodytype} and the
+ weight of this vehicle is ${vehicleweight}, fuel tank capacity is ${fueltankcapacity}, ground clearence 
+ is ${groundclearence}, seating capacity is for ${seatingcapacity} persons, sidemirrors are ${sidemirrors},
+ rims are in ${rimscolor} color, seats are ${seats}, interior is in
+ ${interior} color, horsepower of this car is ${horsepower}, suspension is ${suspension}, headlights 
+ are ${headlights}, there are ${airbags} airbags in this car, if we talk about climate control,
+ climate control is ${climatecontrol}, rearglass is provided in ${rearglass}, frontgrill is
+ ${frontgrill} and the transmission is ${transmission}`);
 
 
 //  this is a black toyota revo with diesel enigine 2.8 the top speed of
