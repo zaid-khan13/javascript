101c27f0bf9e13683876d1e6018d82e4f3c4fb10
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-03-13 16:13:22 -0400

    grading system example
    Co-authored-by: Adil Altaf <adil-innovation-lab@users.noreply.github.com>

---
+++ ifstatement.js
@@ -0,0 +1,45 @@
+// Grading System
+// f: 0-25
+// d: 25-30
+// d+: 30-35
+// c: 35-40
+// c+: 40-50
+// b: 50-60
+// b+: 60-70
+// a: 70-80
+// a+: 80-99
+// a1+: 100
+
+let studentPercentage = 100;
+
+if (studentPercentage < 25) {
+  console.log("F");
+} else if (studentPercentage< 30) {
+  console.log("D");
+} else if (studentPercentage < 35) {
+  console.log("D+");
+} else if (studentPercentage < 40) {
+  console.log("C");
+} else if (studentPercentage < 50) {
+  console.log("C+");
+} else if (studentPercentage < 60) {
+  console.log("B");
+} else if (studentPercentage < 70) {
+  console.log("B+");
+} else if (studentPercentage < 80) {
+  console.log("A");
+} else if (studentPercentage < 99) {
+  console.log("A+");
+} else {
+  console.log("A1+");
+}
+
+
+// New Grading System
+// A+: 90-100
+// A: 80-89
+// B:70-79
+// C: 60-69
+// D: 50-49
+// E: 40-39
+// F: Fail
