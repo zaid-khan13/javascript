0548ebfbd76754812275f4eeaaff1d6ddb8b5c43
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-03-15 16:13:18 -0400

    promptassignment
    Co-authored-by: Adil Altaf <adil-innovation-lab@users.noreply.github.com>

---
+++ prompt.js
@@ -0,0 +1,44 @@
+// To use Prompt in node js
+
+// 1. Install prompt-sync
+// npm install prompt-sync
+
+// 2. import prompt-sync function
+// const ps = require("prompt-sync");
+// const prompt = ps();
+
+const ps = require("prompt-sync");
+const prompt = ps();
+
+// New Grading System
+// A+: 90-100
+// A: 80-89
+// B:70-79
+// C: 60-69
+// D: 50-49
+// E: 40-39
+// F: Fail
+
+let studentPercentage = prompt("whats your percentage?");
+
+if (studentPercentage >= 90) {
+  console.log("A+");
+} else if (studentPercentage >= 80) {
+  console.log("A");
+} else if (studentPercentage >= 70) {
+  console.log("B");
+} else if (studentPercentage > 60) {
+  console.log("C");
+} else if (studentPercentage > 50) {
+  console.log("D");
+} else if (studentPercentage > 40) {
+  console.log("E");
+} else {
+  console.log("F");
+}
+
+// let name = prompt("What's your name?");
+// console.log(`Hello ${name}`);
+
+// let age = prompt("What's your age?");
+// console.log(`${name} is ${age} years old`);
---
+++ promptassignment.js
@@ -0,0 +1,34 @@
+// To use Prompt in node js
+
+// 1. Install prompt-sync
+// npm install prompt-sync
+
+// 2. import prompt-sync function
+// const ps = require("prompt-sync");
+// const prompt = ps();
+
+// 1. Ask user their body type preference
+
+
+// 2. Ask user what brand they like
+
+
+// 3. Ask user what model from that brand
+
+
+// 4. Ask user their color choice
+
+
+// 5. Log the sentence: Ok, I'll find you a {color} {brand} {model} {body type}.
+
+const ps = require("prompt-sync");
+const prompt = ps();
+
+let bodytype = prompt("what type of bodytype you want?");
+
+let brand = prompt("what type of brand you like?");
+
+let model = prompt("what model you like from that brand?");
+
+let color = prompt("what is your color choice?");
+console.log(`ok, I'll find you a ${bodytype} ${brand} ${model} ${color}`);
