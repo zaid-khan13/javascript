09505a38a53bd91f0a5d8d04a2927de0d072e8a8
Zaid Khan <zaidkhaanworld13@gmail.com>
2025-04-15 16:17:49 -0400

    arraysnamelist.js

---
+++ arrays/namelist.js
@@ -0,0 +1,15 @@
+let friends = [
+  "Zaid",
+  "Abdullah",
+  "Mustufa",
+  "Danish",
+  "Bari",
+  "Abdullah",
+  "Hayyan",
+  "Saim",
+  "hassan",
+];
+
+for (let i = 0; i < friends.length; i++) {
+  console.log(`hello ${friends[i]}`);
+}
